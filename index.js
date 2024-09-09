7e514970695f23562f83033fd1b5641910064017
Miguel Placido Felix <164278747+Miguelfelix16@users.noreply.github.com>
2024-09-09 23:03:41 +0000

    09/09/24

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ const nomeCompleto = "Miguel Placido Felix"
 console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos")
 
 anoAtual = anoAtual + 1;
+
 console.log(anoAtual);
 
 idade = idade + 1;
 console.log(idade);
+
+const listaDeDestinos = new Array(
+  "Buenos Aires",
+  "bangkok",
+  "Londres",
+  "Paris",
+  "Nova York",
+)
+
+const idadeComprador = 18;
+
+if(idadeComprador >= 18) {
+  console.log("Comprador maior de idade");
+  console.log("destinos disponíveis: " + listaDeDestinos);
+} else {
+  console.log("Produto Indisponível Para Menores De Idade");
+   console.log("Voce é de moner")
+} 
